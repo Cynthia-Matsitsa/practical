d04233b9be6b025c6120640748bfe6b0ef7077f4
Cynthia <cynthiamatsitsa@gmail.com>
2024-02-27 08:33:11 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var hour=7
+    var rate=500
+    var result=hour*rate
+    println(result)
+
+    var time=5
+    var salary=1000
+    var solution=time*salary
+    println(solution)
+
+}
